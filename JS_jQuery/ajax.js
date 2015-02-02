517919d5546ba41b6446748c5715cc71abcd8b20
Ben Jones <benjaminpeterjonesesquire@gmail.com>
2015-02-02 13:03:19 +0000

    ajax styling

--- JS_jQuery/ajax.js
+++ JS_jQuery/ajax.js
@@ -1,16 +1,65 @@
+url = 'https://www.googleapis.com/books/v1/volumes';
+
 $(document).ready(function() {
-    url = 'https://www.googleapis.com/books/v1/volumes';
-    $.ajax({
-        url: url,
-        data: { q: "sausages"},
-        success: function(data,s,t) {
-            console.log(data);
-            console.log(data.items[0].volumeInfo.title)
-            console.log(data.items[0].volumeInfo.subtitle)
-        },
-        error: function(XMLHttpRequest, textStatus, errorThrown) {
-            alert("Status: " + textStatus);
-            alert("Error: " + errorThrown);
-        }
+    $('form').submit(function(e) {
+    	e.preventDefault();
+    	var query = $('input[name=query]').val();
+
+    	//For styling of elements
+    	$('body').addClass('loading');
+
+	    $.ajax({
+	        url: url,
+	        data: {
+	            q: query
+	        },
+	        success: function(data, s, t) {
+	        	$('body').removeClass('loading');
+	        	$('body').addClass('loaded');
+
+	            console.log(data);
+	            writeBooks(data.items);
+	        },
+	        error: function(XMLHttpRequest, textStatus, errorThrown) {
+	            alert("Status: " + textStatus);
+	            alert("Error: " + errorThrown);
+	        }
+	    })
     })
 })
+
+function writeBooks(items){
+
+	//Define the selector where content will be updated
+	var $b = $('#books tbody');
+
+	var html = '';
+
+	//Remove anything in the #books div
+	$b.html('');
+	for (i in items){
+		item = items[i];
+
+		var title = item.volumeInfo.title;
+		var subtitle = item.volumeInfo.subtitle || ''; // returns '' if undefined
+		var author = item.volumeInfo.authors[0];
+
+		var row = '';
+		row += wrapCell(title);
+		row += wrapCell(subtitle);
+		row += wrapCell(author);
+
+		html += wrapRow(row);
+	}
+
+	$b.append(html);
+}
+
+
+function wrapCell(text){
+	return '<td>' + text + '</td>';
+}
+
+function wrapRow(text){
+	return '<tr>' + text + '</td>';
+}
