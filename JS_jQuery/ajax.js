18a10d4e7241172fbc32ff1fa639b9c805fa75f7
Ben Jones <benjaminpeterjonesesquire@gmail.com>
2015-02-02 11:07:58 +0000

    data parameters

--- JS_jQuery/ajax.js
+++ JS_jQuery/ajax.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
-    url = 'https://www.googleapis.com/books/v1/volumes?q=ghana';
+    url = 'https://www.googleapis.com/books/v1/volumes';
     $.ajax({
         url: url,
+        data: { q: "sausages"},
         success: function(data,s,t) {
             console.log(data);
+            console.log(data.items[0].volumeInfo.title)
             console.log(data.items[0].volumeInfo.subtitle)
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
