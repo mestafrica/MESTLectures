8d86f19833d5f61e1c718e1c819a709bc4e20b69
Ben Jones <benjaminpeterjonesesquire@gmail.com>
2015-02-02 11:04:27 +0000

    extra console line

--- JS_jQuery/ajax.js
+++ JS_jQuery/ajax.js
@@ -2,10 +2,9 @@ $(document).ready(function() {
     url = 'https://www.googleapis.com/books/v1/volumes?q=ghana';
     $.ajax({
         url: url,
-        success: function(data,status,settings) {
+        success: function(data,s,t) {
             console.log(data);
-            console.log(status);
-            console.log(settings);
+            console.log(data.items[0].volumeInfo.subtitle)
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
             alert("Status: " + textStatus);
