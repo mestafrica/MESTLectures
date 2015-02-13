7b6d151f10baf1e5c9ec4f247ebd7adfc0057245
Ben Jones <benjaminpeterjonesesquire@gmail.com>
2015-02-13 14:21:41 +0000

    Styling & validation

--- Meteor/things/client/main.js
+++ Meteor/things/client/main.js
@@ -1,12 +1,13 @@
 Template.registerHelper('things',function(){
 	console.log(Things.find().fetch());
 
-	return Things.find().fetch();
+	// Array is 'naturally' returned by date created. Now the order's reversed.
+	return Things.find().fetch().reverse();
 })
 
-Template.things.events({
+Template.form.events({
 	'submit form': function (e,t) {
-		
+
 		//Prevent page from reloading
 		e.preventDefault();
 
@@ -14,7 +15,12 @@ Template.things.events({
 			name: $('input').val()
 		}
 
-		Things.insert(thing);
+		//Check that 'name' is not an empty string
+		if (thing.name){
+
+			//Insert new doc into 'Things' collection
+			Things.insert(thing);
+		}
 
 		//reset form
 		$('input[type=text]').val('');
