9906a988c1a64909b25785bc80d0cdea9edeeb6e
niinyarko <niinyarko@yahoo.com>
2015-02-16 05:57:26 +0000

    added upvote and downvote buttons

--- Meteor/things/client/main.js
+++ Meteor/things/client/main.js
@@ -2,8 +2,23 @@ Template.registerHelper('things',function(){
 	console.log(Things.find().fetch());
 
 	// Array is 'naturally' returned by date created. Now the order's reversed.
-	return Things.find().fetch().reverse();
+
+	return Things.find({}, {sort: {score: -1} });
 })
+
+Template.things.events({
+	"click .upvote": function () {
+			var postId = this._id;
+			Things.update(postId, {$inc: {score: 1} });
+	},
+
+	"click .downvote": function () {
+		var postId = this._id;
+		Things.update(postId, {$inc: {score: -1} });
+	}
+
+});
+
 
 Template.form.events({
 	'submit form': function (e,t) {
@@ -11,8 +26,13 @@ Template.form.events({
 		//Prevent page from reloading
 		e.preventDefault();
 
+		/*	var item = db.things.find().sort( { score : 1 } ).limit(1);
+			var item_score = item.score;
+			var count_items = db.things.find( { score: { $gt: item_score } } ).count();*/
+
 		thing = {
-			name: $('input').val()
+			name: $('input').val(),
+			score: 0
 		}
 
 		//Check that 'name' is not an empty string
