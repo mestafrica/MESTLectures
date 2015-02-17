357e475bf07296bb6bc79b16451063cc990dbfe8
Ben Jones <benjaminpeterjonesesquire@gmail.com>
2015-02-17 15:40:19 +0000

    added comments to router file

--- Meteor/projects/both/router.js
+++ Meteor/projects/both/router.js
@@ -3,10 +3,10 @@ Router.configure({
   routeControllerNameConverter: "camelCase"
 })
 
-Router.route('/', function () {
-  this.render('home');
+Router.route('/', function () {      // Route is the path after the url
+  this.render('home');               // 'home' refers to the home template
 }, {
-  name: 'home'
+  name: 'home'                       // name is an arbitrary value. Useful for helpers
 });
 
 Router.route('/projects', function () {
