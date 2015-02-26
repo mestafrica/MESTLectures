5f5f4cf9ee55a5f0a5fe39a1a7cb7d6f19109e08
Ben Jones <benjaminpeterjonesesquire@gmail.com>
2015-02-26 13:40:49 +0000

    dashboard route added to navbar

--- Meteor/projects/client/helpers.js
+++ Meteor/projects/client/helpers.js
@@ -1,3 +1,7 @@
 Template.registerHelper('User',function(){
 	return Meteor.user();
 })
+
+Template.registerHelper('ownProjectsCount',function(){
+	return Projects.find({owner: Meteor.userId()}).count()
+})
