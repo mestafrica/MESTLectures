ab801f16e159201f2d73879e1e207148d7e9966b
Ben Jones <benjaminpeterjonesesquire@gmail.com>
2015-03-13 10:47:39 +0000

    forgot to add config file

---
+++ Meteor/projects/both/config.js
@@ -0,0 +1,20 @@
+Config = {
+	name: 'MEST Projects',
+	categories: [
+		{
+			label: 'JavaScript',
+			value: 'javascript',
+			btnClass: 'success'
+		},
+		{
+			label: 'HTML',
+			value: 'html',
+			btnClass: 'primary'
+		},
+		{
+			label: 'CSS',
+			value: 'css',
+			btnClass: 'info'
+		}
+	]
+}
